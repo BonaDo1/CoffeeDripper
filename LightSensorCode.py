cbc8e3a87a8b27e636fee0ee72270d32e90bdc6d
BonaDo1 <94291643+BonaDo1@users.noreply.github.com>
2022-06-01 10:12:19 +0700

    Update LightSensorCode.py

--- LightSensorCode.py
+++ LightSensorCode.py
@@ -1,43 +1,36 @@
-
 import time
 import board
-import digitalio
 from analogio import AnalogIn
 
-inputPin = AnalogIn(board.A1)
-
-THRESHOLD = 1400
-DELAY_SECONDS = 0.005
-TOTAL_RECORD_TIME = 5000.0
+THRESHOLD = 62000
+DELAY_SECONDS = 0.01
+TOTAL_RECORD_TIME = 2.0
 elapsedTime = 0.0
 oldTime = time.monotonic()
 data = []
 
-led = digitalio.DigitalInOut(board.D13)
-led.switch_to_output()
+# Connect an analog sensor to the A1 port of the board you are using. This means a 3V, GND, and signal pin must all be connected.
+analog_in = AnalogIn(board.A1)
 
+# This function turns the analog reading of the sensor (a number from 0 - 65536) to a voltage.
 def readAnalogPin():
-    return inputPin.value
-
+    return analog_in.value
+    
 def thresholdValue(value):
     if(value > THRESHOLD):
         return 1
     else:
         return 0
-
-
+    
 while (elapsedTime < TOTAL_RECORD_TIME):
     currentTime = time.monotonic()
     pinValue = readAnalogPin()
     recordValue = thresholdValue(pinValue)
-
-    print("{0},{1}".format(elapsedTime, recordValue))
+    #print("{0},{1}".format(elapsedTime, recordValue))
     data.append([elapsedTime,recordValue])
-
+    
     time.sleep(DELAY_SECONDS)
     elapsedTime += currentTime - oldTime
     oldTime = currentTime
-
-    led.value = not led.value
 for row in data:
-    print("{0},{1}".format(row[0], row[1]))
+    print("{0},{1}".format(row[0],row[1]))
